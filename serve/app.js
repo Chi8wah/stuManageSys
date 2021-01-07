9a99f39debd9cdad557a6f444654e4a80f1316e1
Chiwah <986771093@qq.com>
2021-01-08 03:34:15 +0800

    完成用户管理功能

--- serve/app.js
+++ serve/app.js
@@ -24,6 +24,7 @@ const mysql = require('mysql')
 
 // eslint-disable-next-line no-path-concat
 // app.use(express.static(__dirname + '/public')) // 配置静态web目录
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 // 解决跨域问题
@@ -40,7 +41,7 @@ app.get('/login', (req, res) => { // 查询
   res.send('查询')
 })
 
-app.post('/login', async (req, res) => { // 提交
+app.post('/login', async (req, res) => { // 提交登录表单
   console.log(req.body)
   const { username, password } = req.body
   var sql = `SELECT * FROM user WHERE username="${username}"`
@@ -94,7 +95,7 @@ app.put('/login', (req, res) => { // 更新
   res.send('更新')
 })
 
-app.get('/info', (req, res) => { // 查询信息
+app.get('/info', (req, res) => { // 查询个人信息
   const { uid, pow, getuid, searchForm } = req.query
   if (((pow === 'student') && (searchForm === 'teacher')) || ((pow !== 'admin') && (getuid === '1'))) {
     return res.send({
@@ -157,9 +158,8 @@ app.get('/info', (req, res) => { // 查询信息
   })
 })
 
-app.put('/info', (req, res) => { // 更新
+app.put('/info', (req, res) => { // 更新个人信息
   const {
-    uid,
     name,
     num,
     sex,
@@ -204,6 +204,190 @@ app.put('/info', (req, res) => { // 更新
       }
     })
   })
+})
+
+app.get('/user', (req, res) => { // 查询用户列表
+  // select  count(*) from table
+  const { pow } = req.query
+  if (pow === 'student') {
+    return res.send({
+      meta: {
+        status: 424
+      },
+      message: '无权访问用户信息!'
+    })
+  }
+
+  let sql = ''
+  if (pow === 'teacher') {
+    sql = 'SELECT * FROM user WHERE (pow<>"admin")'
+  } else if (pow === 'admin') {
+    sql = 'SELECT * FROM user'
+  }
+
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qazwsx110',
+    database: 'stumanagesys'
+  })
+  connection.connect()
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR 查询用户列表出错 - ', err.message)
+
+      connection.end()
+      return res.send({
+        meta: {
+          status: 425
+        },
+        message: '查询用户列表出错!'
+      })
+    }
+    connection.end()
+
+    return res.send({
+      meta: {
+        status: 200
+      },
+      userlist: result
+    })
+  })
+
+  // res.send('查询')
+})
+
+app.put('/user', (req, res) => { // 更新用户信息
+  const {
+    uid,
+    username,
+    password,
+    pow
+  } = req.body
+
+  const sql = `UPDATE user SET username="${username}",password="${password}",pow="${pow}"  WHERE uid=${uid}`
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qazwsx110',
+    database: 'stumanagesys'
+  })
+  connection.connect()
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR] 修改用户出错 - ', err.message)
+
+      connection.end()
+      return res.send({
+        meta: {
+          status: 424
+        },
+        message: '修改出错!'
+      })
+    }
+    connection.end()
+
+    return res.send({
+      meta: {
+        status: 200
+      }
+    })
+    // res.send('更新')
+  })
+})
+
+app.post('/user', (req, res) => { // 添加用户
+  const {
+    username,
+    password,
+    pow
+  } = req.body
+
+  let total = 0
+  const totalsql = 'SELECT COUNT(*) FROM user'
+
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qazwsx110',
+    database: 'stumanagesys'
+  })
+
+  connection.connect()
+  // 查询用户总数
+  connection.query(totalsql, (err, result) => {
+    if (err) {
+      console.log('[SELECT ERROR 查询用户总数出错 - ', err.message)
+
+      connection.end()
+      return res.send({
+        meta: {
+          status: 424
+        },
+        message: '查询总数出错!'
+      })
+    }
+    const { 'COUNT(*)': tot } = result[0]
+    total = tot
+
+    // 添加用户
+    const sql = `INSERT INTO user(uid,username,password,pow) VALUES ("${total + 1}","${username}","${password}","${pow}")`
+    connection.query(sql, function (err, result) {
+      if (err) {
+        console.log('[SELECT ERROR] 添加用户出错 - ', err.message)
+
+        connection.end()
+        return res.send({
+          meta: {
+            status: 425
+          },
+          message: '修改添加用户出错!'
+        })
+      }
+      connection.end()
+
+      return res.send({
+        meta: {
+          status: 200
+        }
+      })
+    })
+  })
+  // res.send('添加用户')
+})
+
+app.delete('/user', (req, res) => { // 删除
+  const { uid } = req.body
+
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qazwsx110',
+    database: 'stumanagesys'
+  })
+
+  const sql = `DELETE FROM user WHERE uid="${uid}"`
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR] 删除用户出错 - ', err.message)
+
+      connection.end()
+      return res.send({
+        meta: {
+          status: 426
+        },
+        message: '删除用户出错!'
+      })
+    }
+    connection.end()
+
+    return res.send({
+      meta: {
+        status: 200
+      }
+    })
+  })
+  // res.send('删除')
 })
 
 app.listen(3000)
--- src/components/user/Subuser.vue
+++ src/components/user/Subuser.vue
@@ -1,13 +1,280 @@
 <template>
-  <div>Subuser</div>
+  <div>
+    <!--    搜索与添加区域-->
+    <el-button type="primary" @click="addDialogVisible = true">
+          添加用户
+    </el-button>
+    <!--      用户列表区-->
+    <el-table
+      class="userTable"
+      :data="userlist"
+      :border="true"
+      :stripe="true">
+      <el-table-column
+        prop="uid"
+        label="uid"
+      ></el-table-column>
+      <el-table-column
+        prop="username"
+        label="姓名"
+      ></el-table-column>
+      <el-table-column
+        prop="password"
+        label="密码"
+      ></el-table-column>
+      <el-table-column
+        prop="pow"
+        label="身份"
+      ></el-table-column>
+      <el-table-column
+        label="操作"
+        width="180px">
+        <template slot-scope="scope">
+          <!--            修改按钮-->
+          <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row)"></el-button>
+          <!--            删除按钮-->
+          <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeUserById(scope.row.uid)"></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!--    添加用户的对话框-->
+    <el-dialog
+      title="添加用户"
+      :visible.sync="addDialogVisible"
+      width="50%"
+      @close="addDialogClosed">
+      <!--      内容主体区-->
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="身份">
+          <el-radio-group v-model="addForm.pow">
+            <el-radio label="teacher">教师</el-radio>
+            <el-radio label="student" :disabled="addPowFlag">学生</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+      <!--      底部区-->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!--    修改用户的对话框-->
+    <el-dialog
+      title="修改用户"
+      :visible.sync="editDialogVisible"
+      width="50%">
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="70px">
+        <el-form-item label="uid">
+          <el-input v-model="editForm.uid" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="editForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="editForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="身份">
+          <el-radio-group v-model="editForm.pow">
+            <el-radio label="teacher">教师</el-radio>
+            <el-radio label="student">学生</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editUserInfo">确 定</el-button>
+      </span>
+    </el-dialog>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Subuser'
+  name: 'Subuser',
+  data () {
+    return {
+      uid: '',
+      pow: '',
+      userlist: [],
+      // 控制添加用户对话框的显示隐藏
+      addDialogVisible: false,
+      addPowFlag: false,
+      addForm: {
+        username: '',
+        password: '',
+        pow: 'student'
+      },
+      addFormRules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          }, {
+            min: 3,
+            max: 10,
+            message: '用户名长度在3~10字符之间',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          }, {
+            min: 6,
+            max: 15,
+            message: '密码长度在6~15字符之间',
+            trigger: 'blur'
+          }
+        ]
+      },
+      // 控制修改用户对话框的显示
+      editDialogVisible: false,
+      // 查询到的用户信息对象
+      editForm: {
+        username: '',
+        password: '',
+        uid: '',
+        pow: ''
+      },
+      editFormRules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          }, {
+            min: 3,
+            max: 10,
+            message: '用户名长度在3~10字符之间',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          }, {
+            min: 6,
+            max: 15,
+            message: '密码长度在6~15字符之间',
+            trigger: 'blur'
+          }
+        ]
+      }
+    }
+  },
+  created () {
+    this.setInfo()
+    this.getUserList()
+  },
+  methods: {
+    setInfo () {
+      this.uid = window.sessionStorage.getItem('uid')
+      this.pow = window.sessionStorage.getItem('pow')
+      if (this.pow === 'teacher') {
+        this.addPowFlag = true
+      }
+    },
+    async getUserList () {
+      const { data: res } = await this.$http.get('user', {
+        params: {
+          pow: this.pow
+        }
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.message)
+      }
+      this.userlist = res.userlist
+    },
+    addDialogClosed () {
+      this.$refs.addFormRef.resetFields()
+    },
+    addUser () {
+      this.$refs.addFormRef.validate(async valid => {
+        if (!valid) return
+        // 可以发起添加用户的网络请求
+        const { data: res } = await this.$http.post('user', this.addForm)
+        if (res.meta.status !== 200) {
+          this.$message.error(res.message)
+        } else {
+          this.$message.success('添加用户成功')
+        }
+        // 隐藏添加用户的对话框
+        this.addDialogVisible = false
+        // 重新获取用户列表
+        this.getUserList()
+      })
+    },
+    showEditDialog (editForm) {
+      this.editForm.username = editForm.username
+      this.editForm.password = editForm.password
+      this.editForm.uid = editForm.uid
+      this.editForm.pow = editForm.pow
+      this.editDialogVisible = true
+    },
+    editUserInfo () {
+      this.$refs.editFormRef.validate(async valid => {
+        if (!valid) return
+        // 发起修改请求
+        const { data: res } = await this.$http.put('user', this.editForm)
+        if (res.meta.status !== 200) {
+          this.$message.error(res.message)
+        }
+        // 关闭对话框
+        this.editDialogVisible = false
+        // 刷新数据列表
+        this.getUserList()
+        // 提示修改成功
+        this.$message.success('更新用户信息成功')
+      })
+    },
+    async removeUserById (uid) {
+      // 不可删除自己
+      if (uid === this.uid) {
+        return this.$message.error('不可删除自己')
+      }
+      // 弹框确认
+      const confirmResult = await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除')
+      }
+      const { data: res } = await this.$http.delete('user', {
+        data: { // 隐式用data, 显式用params
+          uid: uid
+        }
+      })
+      if (res.meta.status !== 200) {
+        this.$message.error(res.message)
+      }
+      this.$message.success('删除用户成功')
+      this.getUserList()
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+.userTable {
+  border-radius: 15px;
+  margin-top: 10px;
+}
 </style>
